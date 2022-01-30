88887d4984b9a76706299bfd72d78372cbeeeb0d
prateek.ksahay <prateek.ksahay@justanswer.com>
2022-01-30 12:10:38 +0530

    Used api to get roles

--- FE/my-app/src/UserManagementToolComponent/UserManagementToolComponent.tsx
+++ FE/my-app/src/UserManagementToolComponent/UserManagementToolComponent.tsx
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { User } from '../Interfaces/Interfaces';
+import { User, UserRoles } from '../Interfaces/Interfaces';
 
 const UserManagementToolComponent = () => {
     const [numberOfUsers, setNumberOfUsers] = useState(0);
     const [userList, setUserList] = useState<User[]>();
+    const [rolesList, setRolesList] = useState<UserRoles[]>();
     useEffect(() => {
-        fetch('https://localhost:44365/api/User')
-        .then(resp => resp.json())
-        .then(data => {            
-        console.log(data);  
-        setNumberOfUsers(data);              
-        }
+            fetch('https://localhost:44365/api/User')
+            .then(resp => resp.json())
+            .then(data => {            
+                console.log(data);  
+                setNumberOfUsers(data);              
+            }
         );
 
         fetch('https://localhost:44365/api/User/all')
-        .then(resp => resp.json())
-        .then(data => {            
-        console.log(data);     
-        setUserList(data);           
-        }
+            .then(resp => resp.json())
+            .then(data => {            
+                console.log(data);     
+                setUserList(data);           
+            }
+        );
+
+        fetch('https://localhost:44365/api/User/roles')
+            .then(resp => resp.json())
+            .then(data => {            
+                console.log(data);     
+                setRolesList(data);           
+            }
         );
     }, [])
 
